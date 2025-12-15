e7dc2ff738e6104b8d0eac90b044f155f57496a4
metaxiii <16118999+metaXIII@users.noreply.github.com.>
2025-12-15 17:22:56 +0100

    fix: fix renovate spring boot upgrade v4 (major)

--- spring-batch/src/test/java/com/metaxiii/fr/springbatch/service/impl/PersonServiceImplTest.java
+++ spring-batch/src/test/java/com/metaxiii/fr/springbatch/service/impl/PersonServiceImplTest.java
@@ -1,15 +1,15 @@
 package com.metaxiii.fr.springbatch.service.impl;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 import com.metaxiii.fr.springbatch.service.PersonService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-
 @DataJpaTest
-@ComponentScan(basePackages ="com.metaxiii.fr.springbatch.service")
+@ComponentScan(basePackages = "com.metaxiii.fr.springbatch.service")
 class PersonServiceImplTest {
 
   @Autowired
